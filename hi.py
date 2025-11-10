d4c4373a78194f2297e3d7fd3a16eb562e74a097
surenthamina <surenthamina@gmail.com>
2025-11-10 18:51:49 +0545

    commit

--- hi.py
+++ hi.py
@@ -2,7 +2,8 @@ try:
     num1 = float(input("Enter first number: "))
     num2 = float(input("Enter second number: "))
     print("Sum:", num1 + num2)
-        if num2 != 0:
+    print("Product:", num1 * num2)
+    if num2 != 0:
         print("Quotient:", num1 / num2)
     else:
         print("Error: Division by zero is not allowed.")
